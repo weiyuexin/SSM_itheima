c1f7f6e8955edf340c4363db7a4774fbc46c765e
weiyuexin <3022422894@qq.com>
2023-01-27 22:29:58 +0800

    auto ware

---
+++ spring_07_autoware/src/main/java/top/weiyuexin/dao/BookDao.java
@@ -0,0 +1,13 @@
+package top.weiyuexin.dao;
+
+/**
+ * @PackageName: top.weiyuexin.dao
+ * @ProjectName: SSM_itheima
+ * @ClassName: BookDao
+ * @Author: Weiyuexin
+ * @Email: 3022422894@qq.com
+ * @Date: 2023/1/21 21:33
+ */
+public interface BookDao {
+    public void save();
+}
---
+++ spring_07_autoware/src/main/java/top/weiyuexin/service/BookService.java
@@ -0,0 +1,13 @@
+package top.weiyuexin.service;
+
+/**
+ * @PackageName: top.weiyuexin.service
+ * @ProjectName: SSM_itheima
+ * @ClassName: BookService
+ * @Author: Weiyuexin
+ * @Email: 3022422894@qq.com
+ * @Date: 2023/1/21 21:35
+ */
+public interface BookService {
+    public void save();
+}
